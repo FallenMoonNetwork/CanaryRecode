cc0622a9ba622f3c770796f88f6f0f50d4df74f2
Jos Kuijpers <jos@kuijpersvof.nl>
2012-05-17 01:35:13 +0200

    Adding a call to the plugin loader. [UNFINISED]

--- src/net/canarymod/Canary.java
+++ src/net/canarymod/Canary.java
@@ -1,11 +1,16 @@
 package net.canarymod;
 
+import java.util.logging.Logger;
+
 import net.canarymod.backbone.IBackbone;
 import net.canarymod.backbone.IBackbone.System;
 import net.canarymod.config.Configuration;
+import net.canarymod.plugin.PluginLoader;
 public class Canary extends ICanary {
 
     private static Canary instance;
+    private PluginLoader loader;
+    private static final Logger log = Logger.getLogger("Minecraft");
     
     /**
      * Private Canary Constructor to prevent more than once instance
@@ -14,6 +19,8 @@ public class Canary extends ICanary {
      */
     private Canary() {
         this.configuration = new Configuration();
+        this.loader = new PluginLoader();
+        this.loader.loadPlugins();
     }
     /**
      * Get a Canary which contains access to all relevant subsystems
