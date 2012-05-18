18ca927be1b2cd2b9d3e31358b7abb4979aae36c
Jos Kuijpers <jos@kuijpersvof.nl>
2012-05-18 11:57:56 +0200

    Re-adding (pre)loading plugins in the chunk provider server,

--- src/net/minecraft/server/OChunkProviderServer.java
+++ src/net/minecraft/server/OChunkProviderServer.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import net.canarymod.Canary;
 import net.canarymod.api.world.ChunkProviderServer;
 import net.minecraft.server.OChunk;
 import net.minecraft.server.OChunkCoordIntPair;
@@ -85,6 +86,12 @@ public class OChunkProviderServer implements OIChunkProvider {
         this.b.remove(Long.valueOf(var3));
         OChunk var5 = (OChunk) this.f.a(var3);
         if (var5 == null) {
+        	// CanaryMod start: load preload plugins
+        	if(loadStage < 1) {
+        		Canary.get().getLoader().loadPlugins(true);
+        		loadStage = 1;
+        	}
+        	// CanaryMod end
             var5 = this.e(var1, var2);
             if (var5 == null) {
                 if (this.d == null) {
@@ -164,6 +171,13 @@ public class OChunkProviderServer implements OIChunkProvider {
     public boolean a(boolean var1, OIProgressUpdate var2) throws IOException {
         int var3 = 0;
 
+        // CanaryMod start: load plugins
+        if(loadStage < 2) {
+        	Canary.get().getLoader().loadPlugins(false);
+        	loadStage = 2;
+        }
+        // CanaryMod end
+        
         for (int var4 = 0; var4 < this.g.size(); ++var4) {
             OChunk var5 = (OChunk) this.g.get(var4);
             if (var1) {
