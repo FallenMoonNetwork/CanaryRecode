d5206e3077f5731730d694cddf0f3ddc745a435d
Jason Jones <darkdiplomat@visualillusionsent.net>
2012-05-24 12:59:12 -0500

    added BiomeData to CanaryChunk

--- src/net/canarymod/api/world/CanaryChunk.java
+++ src/net/canarymod/api/world/CanaryChunk.java
@@ -83,4 +83,51 @@ public class CanaryChunk implements Chunk {
         return null;
     }
 
+    @Override
+    public byte[] getBiomeByteData() {
+        return handle.l();
+    }
+
+    @Override
+    public BiomeType[] getBiomeData() {
+        byte[] data = handle.l();
+        BiomeType[] typedata = new BiomeType[256];
+        for(int index = 0; index < 256; index++){
+            if(data[index] < 0 || data[index] > 22){
+                typedata[index] = BiomeType.OCEAN;
+                continue;
+            }
+            typedata[index] = BiomeType.fromId(data[index]);
+        }
+        return typedata;
+    }
+
+    @Override
+    public void setBiomeData(BiomeType[] type) {
+        if(type.length != 256){
+            return;
+        }
+        byte[] data = new byte[256];
+        for(int index = 0; index < 256; index++){
+            if(type[index] == null){
+                data[index] = (byte)0;
+                continue;
+            }
+            data[index] = type[index].getId();
+        }
+        handle.a(data);
+    }
+
+    @Override
+    public void setBiomeData(byte[] data) {
+        if(data.length != 256){
+            return;
+        }
+        for(byte check : data){
+            if(check < 0 || check > 22){
+                check = (byte)0;
+            }
+        }
+        handle.a(data);
+    }
 }
