c80175651278c2609495e193b0703ef147d1989b
Jos Kuijpers <jos@kuijpersvof.nl>
2012-06-02 17:48:03 +0200

    Instantiating database in CanaryMod.
    Note that it is flatfile only, and does not check the value in the config, yet.

--- src/net/canarymod/CanaryMod.java
+++ src/net/canarymod/CanaryMod.java
@@ -2,6 +2,8 @@ package net.canarymod;
 
 import net.canarymod.backbone.Backbone;
 import net.canarymod.config.Configuration;
+import net.canarymod.database.Database;
+import net.canarymod.database.DatabaseFlatfile;
 import net.canarymod.hook.HookExecutor;
 import net.canarymod.plugin.PluginLoader;
 
@@ -18,6 +20,7 @@ public class CanaryMod extends Canary {
      */
     public CanaryMod() {
     	this.config = new Configuration(); 
+    	this.database = new DatabaseFlatfile();
     	
         // Initialize the loader and scan for plugins
         this.hookExecutor = new HookExecutor();
