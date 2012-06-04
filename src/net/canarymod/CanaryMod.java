fe4b587db28f4ad998ad258415048766c8d9d5aa
Chris Ksoll <chris@playblackstudios.net>
2012-06-04 02:25:03 +0200

    - changes in CanaryMod.java

--- src/net/canarymod/CanaryMod.java
+++ src/net/canarymod/CanaryMod.java
@@ -1,9 +1,12 @@
 package net.canarymod;
 
 import net.canarymod.backbone.Backbone;
+import net.canarymod.bansystem.BanManager;
 import net.canarymod.config.Configuration;
-import net.canarymod.database.Database;
+import net.canarymod.database.Database.Type;
 import net.canarymod.database.DatabaseFlatfile;
+import net.canarymod.database.DatabaseMySql;
+import net.canarymod.group.GroupsProvider;
 import net.canarymod.hook.HookExecutor;
 import net.canarymod.plugin.PluginLoader;
 
@@ -14,22 +17,36 @@ import net.canarymod.plugin.PluginLoader;
  * @author Jos Kuijpers
  */
 public class CanaryMod extends Canary {
-    
+
     /**
      * Creates a new CanaryMod
      */
     public CanaryMod() {
-    	this.config = new Configuration(); 
-    	this.database = new DatabaseFlatfile();
-    	
+        this.config = new Configuration();
+        Type backend = Configuration.getServerConfig().getDatasourceType();
+        if (backend == Type.FLATFILE) {
+            this.database = new DatabaseFlatfile();
+        } 
+        else if (backend == Type.MYSQL) {
+            this.database = new DatabaseMySql();
+        } 
+        else {
+            //Uh oh ...
+            Logman.logWarning("The specified datasource is invalid! Using Flatfile as default.");
+            this.database = new DatabaseFlatfile();
+        }
+
         // Initialize the loader and scan for plugins
         this.hookExecutor = new HookExecutor();
+        this.banManager = new BanManager(database);
+        this.groupsProvider = new GroupsProvider(database);
+        
         this.loader = new PluginLoader();
         this.loader.scanPlugins();
     }
 
     @Override
-    public Backbone getBackbone(Backbone.System arg0) {
+    public Backbone getBackbone(Backbone.System system) {
         // TODO Auto-generated method stub
         return null;
     }
