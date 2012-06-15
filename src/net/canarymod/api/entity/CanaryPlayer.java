29256179aefa230b28b049c49aa0b7f3e471ec84
Chris Ksoll <chris@playblackstudios.net>
2012-06-15 20:24:06 +0200

    - fixed imports according to API changes

--- src/net/canarymod/api/entity/CanaryPlayer.java
+++ src/net/canarymod/api/entity/CanaryPlayer.java
@@ -2,7 +2,6 @@ package net.canarymod.api.entity;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
