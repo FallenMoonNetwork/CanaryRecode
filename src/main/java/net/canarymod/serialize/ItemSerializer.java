0e26489bd56d1b7ffdeeafc100784197be9eda0d
damagefilter <chris@playblack.net>
2013-03-24 14:32:17 +0100

    Changes in ItemSerializer

--- src/main/java/net/canarymod/serialize/ItemSerializer.java
+++ src/main/java/net/canarymod/serialize/ItemSerializer.java
@@ -1,5 +1,6 @@
 package net.canarymod.serialize;
 
+import net.canarymod.Canary;
 import net.canarymod.CanaryDeserializeException;
 import net.canarymod.Logman;
 import net.canarymod.api.CanaryEnchantment;
@@ -13,15 +14,15 @@ public class ItemSerializer implements Serializer<CanaryItem> {
     @Override
     public CanaryItem deserialize(String data) throws CanaryDeserializeException {
         int id, meta, amount, slot;
-
+        
         String[] split = data.split("#");
-        String[] item = split[0].split(";"); // The serialized item
+        String[] item = split[0].split(";"); //The serialized item
         String[] enchantments = null;
-        if (split.length == 2) {
-            enchantments = split[1].split(","); // CSV list of serialized enchantments
+        if(split.length == 2) {
+            enchantments = split[1].split(","); //CSV list of serialized enchantments
         }
-        if (item.length < 4) {
-            throw new CanaryDeserializeException("Could not deserialize Item. Expected fields 4. Found: " + item.length, getVendor());
+        if(item.length < 4) {
+            throw new CanaryDeserializeException("Could not deserialize Item. Expected fields 4. Found: "+item.length, getVendor());
         }
         id = parseInt(item[0]);
         meta = parseInt(item[1]);
@@ -29,9 +30,9 @@ public class ItemSerializer implements Serializer<CanaryItem> {
         slot = parseInt(item[3]);
         CanaryItem citem = new CanaryItem(new OItemStack(id, amount, meta));
         citem.setSlot(slot);
-        if (enchantments != null) {
+        if(enchantments != null) {
             EnchantmentSerializer es = new EnchantmentSerializer();
-            for (String e : enchantments) {
+            for(String e : enchantments) {
                 citem.addEnchantment(es.deserialize(e));
             }
         }
@@ -40,23 +41,23 @@ public class ItemSerializer implements Serializer<CanaryItem> {
 
     @Override
     public String serialize(CanaryItem obje) {
-        if (!(obje instanceof CanaryItem)) {
-            Logman.logInfo("Received object type does not match. Expected CanaryItem. Found: " + obje.getClass().getSimpleName());
+        if(!(obje instanceof CanaryItem)) {
+            Canary.logInfo("Received object type does not match. Expected CanaryItem. Found: "+obje.getClass().getSimpleName());
             return null;
         }
         CanaryItem obj = (CanaryItem) obje;
         StringBuilder fieldsItem = new StringBuilder();
         StringBuilder fieldsEnchants = new StringBuilder();
-        // Process the item data:
+        //Process the item data:
         fieldsItem.append(obj.getId()).append(";")
-                .append(obj.getDamage()).append(";")
-                .append(obj.getAmount()).append(";")
-                .append(obj.getSlot());
-        // Process items enchantments
+            .append(obj.getDamage()).append(";")
+            .append(obj.getAmount()).append(";")
+            .append(obj.getSlot());
+        //Process items enchantments
         EnchantmentSerializer es = new EnchantmentSerializer();
         Enchantment[] enchs = obj.getEnchantments();
-        if (enchs != null) {
-            for (Enchantment ench : obj.getEnchantments()) {
+        if(enchs != null) {
+            for(Enchantment ench : obj.getEnchantments()) {
                 fieldsEnchants.append(es.serialize((CanaryEnchantment) ench)).append(",");
             }
             fieldsItem.append("#").append(fieldsEnchants);
@@ -68,12 +69,13 @@ public class ItemSerializer implements Serializer<CanaryItem> {
     public String getVendor() {
         return "CanaryMod";
     }
-
+    
     private int parseInt(String s) {
         int ret = -1;
         try {
             ret = Integer.parseInt(s);
-        } catch (NumberFormatException e) {
+        }
+        catch(NumberFormatException e) {
             ret = -1;
         }
         return ret;
