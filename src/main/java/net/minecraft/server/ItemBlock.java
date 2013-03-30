a8104ac0973d23f8f5982fa97a8a28c16867f1d8
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-03-30 18:31:09 -0500

    Would like to know block damage value huh?

--- src/main/java/net/minecraft/server/ItemBlock.java
+++ src/main/java/net/minecraft/server/ItemBlock.java
@@ -68,7 +68,7 @@ public class ItemBlock extends Item {
             if (world.f(i0, i1, i2, this.a, i6, 3)) {
                 if (!handled) { // if ItemSlab didn't call BlockPlace
                     // set placed
-                    CanaryBlock placed = new CanaryBlock((short) this.a, (short) 0, i0, i1, i2, world.getCanaryWorld());
+                    CanaryBlock placed = new CanaryBlock((short) this.a, (short) i5, i0, i1, i2, world.getCanaryWorld());
                     // Create and Call
                     BlockPlaceHook hook = new BlockPlaceHook(((EntityPlayerMP) entityplayer).getPlayer(), clicked, placed);
                     Canary.hooks().callHook(hook);
