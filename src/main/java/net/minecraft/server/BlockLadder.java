be36ce383e97c159a765c2a68f44b7c1a403a3f8
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-05-04 03:42:47 -0500

    bounding box fixes for ladders and cobblewalls

---
+++ src/main/java/net/minecraft/server/BlockLadder.java
@@ -0,0 +1,110 @@
+package net.minecraft.server;
+
+import java.util.Random;
+
+public class BlockLadder extends Block {
+
+    protected BlockLadder(int i0) {
+        super(i0, Material.q);
+        this.a(CreativeTabs.c);
+    }
+
+    public AxisAlignedBB b(World world, int i0, int i1, int i2) {
+        this.a((IBlockAccess) world, i0, i1, i2);
+        return super.b(world, i0, i1, i2);
+    }
+
+    public void a(IBlockAccess iblockaccess, int i0, int i1, int i2) {
+        this.c(iblockaccess.h(i0, i1, i2));
+    }
+
+    public void c(int i0) {
+        float f0 = 0.125F;
+
+        if (i0 == 2) {
+            this.a(0.0F, 0.0F, 1.0F - f0, 1.0F, 1.0F, 1.0F);
+        }
+
+        if (i0 == 3) {
+            this.a(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, f0);
+        }
+
+        if (i0 == 4) {
+            this.a(1.0F - f0, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+        }
+
+        if (i0 == 5) {
+            this.a(0.0F, 0.0F, 0.0F, f0, 1.0F, 1.0F);
+        }
+    }
+
+    public boolean c() {
+        return false;
+    }
+
+    public boolean b() {
+        return false;
+    }
+
+    public int d() {
+        return 8;
+    }
+
+    public boolean c(World world, int i0, int i1, int i2) {
+        return world.u(i0 - 1, i1, i2) ? true : (world.u(i0 + 1, i1, i2) ? true : (world.u(i0, i1, i2 - 1) ? true : world.u(i0, i1, i2 + 1)));
+    }
+
+    public int a(World world, int i0, int i1, int i2, int i3, float f0, float f1, float f2, int i4) {
+        int i5 = i4;
+
+        if ((i4 == 0 || i3 == 2) && world.u(i0, i1, i2 + 1)) {
+            i5 = 2;
+        }
+
+        if ((i5 == 0 || i3 == 3) && world.u(i0, i1, i2 - 1)) {
+            i5 = 3;
+        }
+
+        if ((i5 == 0 || i3 == 4) && world.u(i0 + 1, i1, i2)) {
+            i5 = 4;
+        }
+
+        if ((i5 == 0 || i3 == 5) && world.u(i0 - 1, i1, i2)) {
+            i5 = 5;
+        }
+
+        return i5;
+    }
+
+    public void a(World world, int i0, int i1, int i2, int i3) {
+        int i4 = world.h(i0, i1, i2);
+        boolean flag0 = false;
+
+        if (i4 == 2 && world.u(i0, i1, i2 + 1)) {
+            flag0 = true;
+        }
+
+        if (i4 == 3 && world.u(i0, i1, i2 - 1)) {
+            flag0 = true;
+        }
+
+        if (i4 == 4 && world.u(i0 + 1, i1, i2)) {
+            flag0 = true;
+        }
+
+        if (i4 == 5 && world.u(i0 - 1, i1, i2)) {
+            flag0 = true;
+        }
+
+        if (!flag0) {
+            this.c(world, i0, i1, i2, i4, 0);
+            world.i(i0, i1, i2);
+        }
+
+        super.a(world, i0, i1, i2, i3);
+    }
+
+    public int a(Random random) {
+        return 1;
+    }
+}
---
+++ src/main/java/net/minecraft/server/BlockWall.java
@@ -0,0 +1,92 @@
+package net.minecraft.server;
+
+public class BlockWall extends Block {
+
+    public static final String[] a = new String[] { "normal", "mossy"};
+
+    public BlockWall(int i0, Block block) {
+        super(i0, block.cO);
+        this.c(block.cA);
+        this.b(block.cB / 3.0F);
+        this.a(block.cM);
+        this.a(CreativeTabs.b);
+    }
+
+    public int d() {
+        return 32;
+    }
+
+    public boolean b() {
+        return false;
+    }
+
+    public boolean b(IBlockAccess iblockaccess, int i0, int i1, int i2) {
+        return false;
+    }
+
+    public boolean c() {
+        return false;
+    }
+
+    public void a(IBlockAccess iblockaccess, int i0, int i1, int i2) {
+        boolean flag0 = this.d(iblockaccess, i0, i1, i2 - 1);
+        boolean flag1 = this.d(iblockaccess, i0, i1, i2 + 1);
+        boolean flag2 = this.d(iblockaccess, i0 - 1, i1, i2);
+        boolean flag3 = this.d(iblockaccess, i0 + 1, i1, i2);
+        float f0 = 0.25F;
+        float f1 = 0.75F;
+        float f2 = 0.25F;
+        float f3 = 0.75F;
+        float f4 = 1.0F;
+
+        if (flag0) {
+            f2 = 0.0F;
+        }
+
+        if (flag1) {
+            f3 = 1.0F;
+        }
+
+        if (flag2) {
+            f0 = 0.0F;
+        }
+
+        if (flag3) {
+            f1 = 1.0F;
+        }
+
+        if (flag0 && flag1 && !flag2 && !flag3) {
+            f4 = 0.8125F;
+            f0 = 0.3125F;
+            f1 = 0.6875F;
+        } else if (!flag0 && !flag1 && flag2 && flag3) {
+            f4 = 0.8125F;
+            f2 = 0.3125F;
+            f3 = 0.6875F;
+        }
+
+        this.a(f0, 0.0F, f2, f1, f4, f3);
+    }
+
+    public AxisAlignedBB b(World world, int i0, int i1, int i2) {
+        this.a((IBlockAccess) world, i0, i1, i2);
+        this.cK = 1.5D;
+        return super.b(world, i0, i1, i2);
+    }
+
+    public boolean d(IBlockAccess iblockaccess, int i0, int i1, int i2) {
+        int i3 = iblockaccess.a(i0, i1, i2);
+
+        if (i3 != this.cz && i3 != Block.bz.cz) {
+            Block block = Block.r[i3];
+
+            return block != null && block.cO.k() && block.b() ? block.cO != Material.A : false;
+        } else {
+            return true;
+        }
+    }
+
+    public int a(int i0) {
+        return i0;
+    }
+}
