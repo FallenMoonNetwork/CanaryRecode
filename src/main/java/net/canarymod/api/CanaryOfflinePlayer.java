4ff9ddb68b776ad79149aba84b0a433d1d6ed12d
damagefilter <chris@playblack.net>
2013-05-30 21:26:54 +0200

    Implemented changes in OfflinePlayer

--- src/main/java/net/canarymod/api/CanaryOfflinePlayer.java
+++ src/main/java/net/canarymod/api/CanaryOfflinePlayer.java
@@ -7,12 +7,16 @@ import net.canarymod.Canary;
 import net.canarymod.api.nbt.CanaryBaseTag;
 import net.canarymod.api.nbt.CanaryCompoundTag;
 import net.canarymod.api.nbt.CanaryDoubleTag;
+import net.canarymod.api.nbt.CompoundTag;
 import net.canarymod.api.nbt.ListTag;
+import net.canarymod.api.world.CanaryWorld;
 import net.canarymod.api.world.DimensionType;
 import net.canarymod.api.world.World;
 import net.canarymod.api.world.position.Position;
 import net.canarymod.permissionsystem.PermissionProvider;
 import net.canarymod.user.Group;
+import net.minecraft.server.ISaveHandler;
+import net.minecraft.server.SaveHandler;
 
 public class CanaryOfflinePlayer implements OfflinePlayer {
 
@@ -134,6 +138,30 @@ public class CanaryOfflinePlayer implements OfflinePlayer {
             return false;
         }
         return removeGroup(gr);
+    }
+    @Override
+    public boolean isOnline() {
+        return Canary.getServer().getPlayer(name) != null;
+    }
+    @Override
+    public CompoundTag getNBT() {
+        return data;
+    }
+    @Override
+    public void save() {
+        if(isOnline()) {
+            Canary.logDebug("Attempted to save an online player! ("+getName()+")");
+            return;
+        }
+        ISaveHandler handler = ((CanaryWorld)getWorld()).getHandle().L();
+        if(handler instanceof SaveHandler) {
+            SaveHandler shandler = (SaveHandler)handler;
+            shandler.writePlayerNbt(getName(), (CanaryCompoundTag)getNBT());
+        }
+        else {
+            Canary.logServerMessage(getName() + "'s OfflinePlayer could not be saved! Unsupported SaveHandler!");
+        }
+
     }
 
 }
