093860dfb1ad3aaff5441437ae1ad5169bc3b750
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-06-30 13:40:15 -0500

    little better error handling in Main

--- src/main/java/net/canarymod/Main.java
+++ src/main/java/net/canarymod/Main.java
@@ -32,39 +32,40 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-
-        MinecraftServer.setHeadless(GraphicsEnvironment.isHeadless());
-        for (int i1 = 0; i1 < args.length; ++i1) {
-            String s3 = args[i1];
-            if (!s3.equals("nogui") && !s3.equals("--nogui")) {
-                ;
-            } else {
-                MinecraftServer.setHeadless(true);
-            }
-        }
-
-        // Initialize Logging Early, TODO: the new File(".") is a directory setting, the cli arg is --universe
-        la = new LogAgent("Minecraft-Server", (String) null, (new File(new File("."), "server.log")).getAbsolutePath());
-        la.a().setLevel(Level.ALL);
-        if (!MinecraftServer.isHeadless()) {
-            GuiLogOutputHandler.getOutputHandler().poke();
-        }
-        initBird();
         try {
+            MinecraftServer.setHeadless(GraphicsEnvironment.isHeadless());
+            for (int i1 = 0; i1 < args.length; ++i1) {
+                String s3 = args[i1];
+                if (!s3.equals("nogui") && !s3.equals("--nogui")) {
+                    ;
+                } else {
+                    MinecraftServer.setHeadless(true);
+                }
+            }
+
+            // Initialize Logging Early, TODO: the new File(".") is a directory setting, the cli arg is --universe
+            la = new LogAgent("Minecraft-Server", (String) null, (new File(new File("."), "server.log")).getAbsolutePath());
+            la.a().setLevel(Level.ALL);
+            if (!MinecraftServer.isHeadless()) {
+                GuiLogOutputHandler.getOutputHandler().poke();
+            }
+            initBird();
+
             MinecraftServer.main(args);
+
+            // They need the server to be set
+            mod.initPermissions();
+            // Initialize providers that require Canary to be set already
+            mod.initUserAndGroupsManager();
+            mod.initKits();
+            // Warps need the DimensionType data which is created upon servre start
+            mod.initWarps();
+            // commands require a valid commandOwner which is the server.
+            // That means for commands to work, we gotta load Minecraft first
+            mod.initCommands();
         } catch (Throwable t) {
             Canary.logStacktrace("Exception while starting the server: ", t);
         }
-        //They need the server to be set
-        mod.initPermissions();
-        // Initialize providers that require Canary to be set already
-        mod.initUserAndGroupsManager();
-        mod.initKits();
-        //Warps need the DimensionType data which is created upon servre start
-        mod.initWarps();
-        // commands require a valid commandOwner which is the server.
-        // That means for commands to work, we gotta load Minecraft first
-        mod.initCommands();
     }
 
     /**
